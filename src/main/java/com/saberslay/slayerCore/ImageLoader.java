b5d280ce87b5e9f968796650ed03130ee4a22df3
Saberslay <dansteamshell@gmail.com>
2026-01-04 15:54:15 +0000

    Update image path and logging in ImageLoader
    Changed image directory from 'res/gfx/' to 'resources/gfx/' and replaced System.err logging with Logger.warn for better logging consistency.

--- src/main/java/com/saberslay/slayerCore/ImageLoader.java
+++ src/main/java/com/saberslay/slayerCore/ImageLoader.java
@@ -16,21 +16,21 @@ import javax.swing.JFrame;
 public class ImageLoader {
     public static void setIcon(JFrame frame, String fileName) {
         try {
-            File file = new File("res/gfx/" + fileName + ".png");
+            File file = new File("resources/gfx/" + fileName + ".png");
             Image icon = ImageIO.read(file);
             frame.setIconImage(icon);
         } catch (IOException e) {
-            System.err.println("Failed to load window icon: " + fileName);
+            Logger.warn("Failed to load window icon: " + fileName);
             e.printStackTrace();
         }
     }
 
     public static BufferedImage loadImage(String fileName) {
         try {
-            File file = new File("res/gfx/" + fileName + ".png");
+            File file = new File("resources/gfx/" + fileName + ".png");
             return ImageIO.read(file);
         } catch (IOException e) {
-            System.err.println("Failed to load image: " + fileName);
+            Logger.warn("Failed to load image: " + fileName);
             e.printStackTrace();
             return null;
         }
