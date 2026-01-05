c8a799fdb43e864d77adbf077da5570c7cd4744f
Saberslay <dansteamshell@gmail.com>
2026-01-05 00:50:33 +0000

    Load images from classpath resources instead of file system
    Refactored ImageLoader to use getResourceAsStream for loading images and icons from the classpath under /gfx/, instead of reading from the file system. This improves portability and allows resources to be bundled within JAR files. Added warnings for missing resources.

--- src/main/java/com/saberslay/slayerCore/ImageLoader.java
+++ src/main/java/com/saberslay/slayerCore/ImageLoader.java
@@ -4,6 +4,7 @@ import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
@@ -13,24 +14,46 @@ import javax.swing.JFrame;
  * Licensed under the MIT License.
  */
 
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.InputStream;
+import java.io.IOException;
+
 public class ImageLoader {
+
+    private static final String BASE_PATH = "/gfx/";
+
     public static void setIcon(JFrame frame, String fileName) {
-        try {
-            File file = new File("resources/gfx/" + fileName + ".png");
-            Image icon = ImageIO.read(file);
+        String path = BASE_PATH + fileName + ".png";
+
+        try (InputStream is = ImageLoader.class.getResourceAsStream(path)) {
+            if (is == null) {
+                Logger.warn("Icon not found on classpath: " + path);
+                return;
+            }
+
+            Image icon = ImageIO.read(is);
             frame.setIconImage(icon);
+
         } catch (IOException e) {
-            Logger.warn("Failed to load window icon: " + fileName);
+            Logger.warn("Failed to load window icon: " + path);
             e.printStackTrace();
         }
     }
 
     public static BufferedImage loadImage(String fileName) {
-        try {
-            File file = new File("resources/gfx/" + fileName + ".png");
-            return ImageIO.read(file);
+        String path = BASE_PATH + fileName + ".png";
+
+        try (InputStream is = ImageLoader.class.getResourceAsStream(path)) {
+            if (is == null) {
+                Logger.warn("Image not found on classpath: " + path);
+                return null;
+            }
+            return ImageIO.read(is);
         } catch (IOException e) {
-            Logger.warn("Failed to load image: " + fileName);
+            Logger.warn("Failed to load image: " + path);
             e.printStackTrace();
             return null;
         }
