095b64b71cb28722eebeb304670e75d4f178f57b
Saberslay <dansteamshell@gmail.com>
2026-02-13 17:12:21 +0000

    Rename writerBytes to writeBytes; add String stub
    Rename multiple overloaded writerBytes methods to writeBytes (byte, short, char, int, long, float, double, boolean) and update internal call sites to use the new name. Add a new writeBytes(byte[], int, String) stub with TODO comments for writing length and null-termination; implementation to be completed. (Note: file currently missing a trailing newline.)

--- src/main/java/com/saberslay/slayerCore/serialization/SerializationWriter.java
+++ src/main/java/com/saberslay/slayerCore/serialization/SerializationWriter.java
@@ -11,25 +11,25 @@ public class SerializationWriter {
     public static final byte[] HEADER = "SC".getBytes();
     public static final short VERSION = 0x0100; // big endian
 
-    public static int writerBytes(byte[] dest, int pointer, byte value) {
+    public static int writeBytes(byte[] dest, int pointer, byte value) {
         dest[pointer++] = value;
         return pointer;
     }
 
-    public static int writerBytes(byte[] dest, int pointer, short value) {
+    public static int writeBytes(byte[] dest, int pointer, short value) {
         dest[pointer++] = (byte)((value >> 8) & 0xff);
         dest[pointer++] = (byte)((value >> 0) & 0xff);
         return pointer;
     }
 
-    public static int writerBytes(byte[] dest, int pointer, char value) {
+    public static int writeBytes(byte[] dest, int pointer, char value) {
         dest[pointer++] = (byte)((value >> 8) & 0xff);
         dest[pointer++] = (byte)((value >> 0) & 0xff);
         return pointer;
     }
 
 
-    public static int writerBytes(byte[] dest, int pointer, int value) {
+    public static int writeBytes(byte[] dest, int pointer, int value) {
         dest[pointer++] = (byte)((value >> 24) & 0xff);
         dest[pointer++] = (byte)((value >> 16) & 0xff);
         dest[pointer++] = (byte)((value >> 8) & 0xff);
@@ -37,7 +37,7 @@ public class SerializationWriter {
         return pointer;
     }
 
-    public static int writerBytes(byte[] dest, int pointer, long value) {
+    public static int writeBytes(byte[] dest, int pointer, long value) {
         dest[pointer++] = (byte)((value >> 56) & 0xff);
         dest[pointer++] = (byte)((value >> 48) & 0xff);
         dest[pointer++] = (byte)((value >> 40) & 0xff);
@@ -49,18 +49,24 @@ public class SerializationWriter {
         return pointer;
     }
 
-    public static int writerBytes(byte[] dest, int pointer, float value) {
+    public static int writeBytes(byte[] dest, int pointer, float value) {
        int data = Float.floatToIntBits(value);
-       return writerBytes(dest, pointer, data);
+       return writeBytes(dest, pointer, data);
     }
 
-    public static int writerBytes(byte[] dest, int pointer, double value) {
+    public static int writeBytes(byte[] dest, int pointer, double value) {
         long data = Double.doubleToLongBits(value);
-        return writerBytes(dest, pointer, data);
+        return writeBytes(dest, pointer, data);
     }
 
-    public static int writerBytes(byte[] dest, int pointer, boolean value) {
+    public static int writeBytes(byte[] dest, int pointer, boolean value) {
         dest[pointer++] = (byte)(value ? 1 : 0);
         return pointer;
+    }
+
+    public static int writeBytes(byte[] dest, int pointer, String string ) {
+        // 1. Write the size before the string 07 sa be rs la y
+        // 2. Null-termination character sa be rs la y 0
+        // 3. Both 06
     }
 }
