63a0f161809ae6bfa64e7b77a3d947227fce43e1
Saberslay <dansteamshell@gmail.com>
2026-02-15 18:17:30 +0000

    Add SC serialization core classes and utils
    Introduce a binary serialization subsystem: add ContainerType and Type constants, SCBase base class, and concrete containers SCField, SCString, SCArray, SCObject and SCDatabase with getBytes/Deserialize implementations and size tracking. Add SerializationUtils with low-level read/write helpers and primitive array readers/writers; SCDatabase supports file I/O and SCDB header/version. Remove the old SerializationWriter in favor of the consolidated utilities.

---
+++ src/main/java/com/saberslay/slayerCore/serialization/SCDatabase.java
@@ -0,0 +1,125 @@
+package com.saberslay.slayerCore.serialization;
+
+import static com.saberslay.slayerCore.serialization.SerializationUtils.*;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SCDatabase extends SCBase {
+
+	public static final byte[] HEADER = "SCDB".getBytes();
+	public static final short VERSION = 0x0100;
+	public static final byte CONTAINER_TYPE = ContainerType.DATABASE;
+	private short objectCount;
+	public List<SCObject> objects = new ArrayList<SCObject>();
+	
+	private SCDatabase() {
+	}
+	
+	public SCDatabase(String name) {
+		setName(name);
+		
+		size += HEADER.length + 2 + 1 + 2;
+	}
+	
+	public void addObject(SCObject object) {
+		objects.add(object);
+		size += object.getSize();
+		
+		objectCount = (short)objects.size();
+	}
+	
+	public int getSize() {
+		return size;
+	}
+
+	public int getBytes(byte[] dest, int pointer) {
+		pointer = writeBytes(dest, pointer, HEADER);
+		pointer = writeBytes(dest, pointer, VERSION);
+		pointer = writeBytes(dest, pointer, CONTAINER_TYPE);
+		pointer = writeBytes(dest, pointer, nameLength);
+		pointer = writeBytes(dest, pointer, name);
+		pointer = writeBytes(dest, pointer, size);
+		
+		pointer = writeBytes(dest, pointer, objectCount);
+		for (SCObject object : objects)
+			pointer = object.getBytes(dest, pointer);
+		
+		return pointer;
+	}
+	
+	public static SCDatabase Deserialize(byte[] data) {
+		int pointer = 0;
+		assert(readString(data, pointer, HEADER.length).equals(HEADER));
+		pointer += HEADER.length;
+		
+		if (readShort(data, pointer) != VERSION) {
+			System.err.println("Invalid SCDB version!");
+			return null;
+		}
+		pointer += 2;
+		
+		byte containerType = readByte(data, pointer++);
+		assert(containerType == CONTAINER_TYPE);
+		
+		SCDatabase result = new SCDatabase();
+		result.nameLength = readShort(data, pointer);
+		pointer += 2;
+		result.name = readString(data, pointer, result.nameLength).getBytes();
+		pointer += result.nameLength;
+		
+		result.size = readInt(data, pointer);
+		pointer += 4;
+		
+		result.objectCount = readShort(data, pointer);
+		pointer += 2;
+		
+		for (int i = 0; i < result.objectCount; i++) {
+			SCObject object = SCObject.Deserialize(data, pointer);
+			result.objects.add(object);
+			pointer += object.getSize(); 
+		}
+		
+		return result;
+	}
+	
+	public SCObject findObject(String name) {
+		for (SCObject object : objects) {
+			if (object.getName().equals(name))
+				return object;
+		}
+		return null;
+	}
+
+	public static SCDatabase DeserializeFromFile(String path) {
+		byte[] buffer = null;
+		try {
+			BufferedInputStream stream = new BufferedInputStream(new FileInputStream(path));
+			buffer = new byte[stream.available()];
+			stream.read(buffer);
+			stream.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return Deserialize(buffer);
+	}
+	
+	public void serializeToFile(String path) {
+		byte[] data = new byte[getSize()];
+		getBytes(data, 0);
+		try {
+			BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(path));
+			stream.write(data);
+			stream.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
---
+++ src/main/java/com/saberslay/slayerCore/serialization/SCObject.java
@@ -0,0 +1,141 @@
+package com.saberslay.slayerCore.serialization;
+
+import static com.saberslay.slayerCore.serialization.SerializationUtils.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SCObject extends SCBase {
+
+	public static final byte CONTAINER_TYPE = ContainerType.OBJECT;
+	private short fieldCount;
+	public List<SCField> fields = new ArrayList<SCField>();
+	private short stringCount;
+	public List<SCString> strings = new ArrayList<SCString>();
+	private short arrayCount;
+	public List<SCArray> arrays = new ArrayList<SCArray>();
+	
+	private SCObject() {
+	}
+	
+	public SCObject(String name) {
+		size += 1 + 2 + 2 + 2;
+		setName(name);
+	}
+	
+	public void addField(SCField field) {
+		fields.add(field);
+		size += field.getSize();
+		
+		fieldCount = (short)fields.size();
+	}
+	
+	public void addString(SCString string) {
+		strings.add(string);
+		size += string.getSize();
+		
+		stringCount = (short)strings.size();
+	}
+
+	public void addArray(SCArray array) {
+		arrays.add(array);
+		size += array.getSize();
+		
+		arrayCount = (short)arrays.size();
+	}
+	
+	public int getSize() {
+		return size;
+	}
+	
+	public SCField findField(String name) {
+		for (SCField field : fields) {
+			if (field.getName().equals(name))
+				return field;
+		}
+		return null;
+	}
+	
+	public SCString findString(String name) {
+		for (SCString string : strings) {
+			if (string.getName().equals(name))
+				return string;
+		}
+		return null;
+	}
+
+	public SCArray findArray(String name) {
+		for (SCArray array : arrays) {
+			if (array.getName().equals(name))
+				return array;
+		}
+		return null;
+	}
+	
+	public int getBytes(byte[] dest, int pointer) {
+		pointer = writeBytes(dest, pointer, CONTAINER_TYPE);
+		pointer = writeBytes(dest, pointer, nameLength);
+		pointer = writeBytes(dest, pointer, name);
+		pointer = writeBytes(dest, pointer, size);
+		
+		pointer = writeBytes(dest, pointer, fieldCount);
+		for (SCField field : fields)
+			pointer = field.getBytes(dest, pointer);
+		
+		pointer = writeBytes(dest, pointer, stringCount);
+		for (SCString string : strings)
+			pointer = string.getBytes(dest, pointer);
+		
+		pointer = writeBytes(dest, pointer, arrayCount);
+		for (SCArray array : arrays)
+			pointer = array.getBytes(dest, pointer);
+		
+		return pointer;
+	}
+	
+	public static SCObject Deserialize(byte[] data, int pointer) {
+		byte containerType = data[pointer++];
+		assert(containerType == CONTAINER_TYPE);
+		
+		SCObject result = new SCObject();
+		result.nameLength = readShort(data, pointer);
+		pointer += 2;
+		result.name = readString(data, pointer, result.nameLength).getBytes();
+		pointer += result.nameLength;
+		
+		result.size = readInt(data, pointer);
+		pointer += 4;
+		
+		// Early-out: pointer += result.size - sizeOffset - result.nameLength;
+		
+		result.fieldCount = readShort(data, pointer);
+		pointer += 2;
+		
+		for (int i = 0; i < result.fieldCount; i++) {
+			SCField field = SCField.Deserialize(data, pointer);
+			result.fields.add(field);
+			pointer += field.getSize();
+		}
+		
+		result.stringCount = readShort(data, pointer);
+		pointer += 2;
+
+		for (int i = 0; i < result.stringCount; i++) {
+			SCString string = SCString.Deserialize(data, pointer);
+			result.strings.add(string);
+			pointer += string.getSize();
+		}
+
+		result.arrayCount = readShort(data, pointer);
+		pointer += 2;
+
+		for (int i = 0; i < result.arrayCount; i++) {
+			SCArray array = SCArray.Deserialize(data, pointer);
+			result.arrays.add(array);
+			pointer += array.getSize();
+		}
+		
+		return result;
+	}
+	
+}
