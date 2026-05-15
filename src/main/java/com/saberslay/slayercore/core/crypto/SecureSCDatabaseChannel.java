99d94febc13cae36611c8403bfa86dad138bbd60
Saberslay <dansteamshell@gmail.com>
2026-05-15 20:48:35 +0100

    Add SecureUDPClientBase and document DB channel
    Introduce SecureUDPClientBase: a base class for encrypted UDP clients using StrongCustomCipher with a background listener thread, start/stop lifecycle, sendSecure, and abstract callbacks for decrypted packets and send/receive errors. Clean up SecureSCDatabaseChannel by relocating the Listener field, removing a duplicate declaration, and adding JavaDoc comments for encode/decode to clarify serialize/encrypt and decrypt/deserialize responsibilities.

--- src/main/java/com/saberslay/slayercore/core/crypto/SecureSCDatabaseChannel.java
+++ src/main/java/com/saberslay/slayercore/core/crypto/SecureSCDatabaseChannel.java
@@ -11,19 +11,21 @@ import com.saberslay.slayercore.core.serialization.SCDatabase;
 public class SecureSCDatabaseChannel {
 
     private final StrongCustomCipher cipher;
+    private final Listener listener;
 
     public interface Listener {
         void onDatabaseReceived(SCDatabase db);
         void onDecryptionFailed(Exception e);
     }
 
-    private final Listener listener;
-
     public SecureSCDatabaseChannel(byte[] masterKey, Listener listener) {
         this.cipher = new StrongCustomCipher(masterKey);
         this.listener = listener;
     }
 
+    /**
+     * Serialize + encrypt an SCDatabase into a secure packet.
+     */
     public byte[] encode(SCDatabase db) {
         try {
             byte[] plain = db.serialize();
@@ -33,6 +35,9 @@ public class SecureSCDatabaseChannel {
         }
     }
 
+    /**
+     * Decrypt + deserialize a received packet.
+     */
     public void decode(byte[] packet) {
         try {
             byte[] decrypted = cipher.decrypt(packet);
